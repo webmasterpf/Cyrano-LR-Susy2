ebe7707c47632582e620e6270f0ded73d161b149
webmasterpf-DG <webmaster@provence-formation.com>
2016-06-29 11:27:19 +0200

    LR: fiche formation

--- node/node-fiche_formations.tpl.php
+++ node/node-fiche_formations.tpl.php
@@ -1,37 +1,26 @@
-<!-- ______________________ CUSTOM NODE.TPL POUR FICHE FORMATION _______________________ -->
-<!-- ______________________ COLONNE GAUCHE _______________________ -->
-
-
-         <div id="left-content-fiche-formations">
-              <?php if ($title): ?>
-                <h1 class="title"><?php print $title; ?></h1>
-              <?php endif; ?>
-                <br clear="all"/>
+<?php
+/* Ce template permet la création d'un layout multicolonne pour les pages de base, en permettant la disposition facile
+ * des champs CCK custom, si nécessaires pour une page de base.
+*/?>
+<!--______________NODE TPL POUR PAGE.TPL CUSTOM________________ -->
+<div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
+    <div class="node-inner">
+        <!--______________COLONNE 1________________ -->
+        <div id="colonne-1" class="col1_layout_3_9 fiche-formation">
+            <?php if ($title): /*copier le titre dans la colonne desirée*/?>
+            <h1 class="titre_ficheform"><?php print $title; ?></h1>
+            <?php endif; ?>
+  
                 <?php if ($node->field_complement_info_formation[0]['view']): ?>
 
                 <div class="complement_titre_fiche"><?php  (print $node->field_complement_info_formation[0]['view']); /*Info complementaire sur formation*/ ?></div>
 
             <?php endif; ?>
 
-                <div id="diapo_fiche">
-                   <?php //foreach ((array)$node->field_diapo_deco as $item){ ?>
-                    <?php //print $item['view'] ?>
-                    <?php //} ?>
-                    <?php //if ($node->field_diapo_deco[0]['view']): ?>
-                        <?php //print $node->field_diapo_deco[0]['view'] ?>
-                    <?php //endif; ?>    
-                    
-                          <?php //affichage diaporama views inclue via CCK
-                          if ($node->field_diapo_ficheform[0]['view']): ?>
-                        <?php print $node->field_diapo_ficheform[0]['view'] ?>
-                    <?php endif; ?>  
-                </div>
-                
-                  
                 
                 
                 <?php if ($node-> field_programme_formation[0]['view'] ):?>
-                <div id="programme">
+                <div class="programme">
                 <?php  print $node->content['field_programme_formation']['field']['#title'] ?>
                 <?php print $node-> field_programme_formation[0]['view'] ?>
                 </div> 
@@ -52,57 +41,45 @@
                ?>  
                   </ul>   </div>
             <?php endif;?>
+            
+                 <?php
+           global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_col_1.php');
+              ?>
+        </div><!-- /colonne1 -->
+        <!--______________COLONNE 2________________ -->
+         <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
+        <div id="colonne-2" class="col2_layout_3_9 fiche-formation">
+
+            <?php print $picture; ?>
+
+            <?php if ($submitted): ?>
+            <span class="submitted"><?php print $submitted; ?></span>
+            <?php endif; ?>
+
+            <div class="content">
                 
-           
+                          <?php //affichage diaporama views inclue via CCK
+                          if ($node->field_diapo_ficheform[0]['view']): ?>
+                   <div class="diapo_fiche">
+                        <?php print $node->field_diapo_ficheform[0]['view'] ?>
+                       </div>
+                    <?php endif; ?>  
+                
+                <?php   print $node->content['body']['#value'];/*déplacer le contenu dans la colonne désirée*/ ?>
+            </div>
 
-                <?php //if ($left): ?>
-                    <?php //print $left; ?>
-                <?php //endif; ?>
-          </div>
-             <!-- /sidebar-left -->
+        </div><!-- /colonne2 -->
 
-		<!--fin du contenu gauche -->
+   
 
+        <?php if ($terms): ?>
+        <div class="taxonomy"><?php //print $terms; ?></div>
+        <?php endif;?>
 
-            <!-- ____________LAYOUT NODE FICHE FORMATION____-->
-             <!-- ______________________ CONTENU CENTRAL _______________________ -->
-          <div id="middle-content-fiche-formations">
-          
-
-<div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
-  <div class="node-inner">
-
-    <?php if (!$page): ?>
-      <h2 class="title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
-
-    <?php print $picture; ?>
-
-    <?php if ($submitted): ?>
-      <span class="submitted"><?php print $submitted; ?></span>
-    <?php endif; ?>
-
-    <div class="content">
-      <?php //print $content; ?>
-<?php print $node->content['body']['#value'] ?>
-    </div>
-
-    <?php if ($terms): ?>
-      <div class="taxonomy"><?php print $terms; ?></div>
-    <?php endif;?>
-
-    <?php if ($links): ?> 
-      <div class="links"> <?php print $links; ?></div>
-    <?php endif; ?>
-
-  </div> <!-- /node-inner -->
-</div> <!-- /node-->
-
-          </div> <!-- /#content-area -->
-<?php if (!empty($primary_links) or !empty($secondary_links)): ?>
-          <div id="navigation" class="menu <?php if (!empty($primary_links)) { print "with-main-menu"; } if (!empty($secondary_links)) { print " with-sub-menu"; } ?>">
-            <?php if (!empty($primary_links)){ print theme('links', $primary_links, array('id' => 'primary', 'class' => 'links main-menu')); } ?>
-            <?php if (!empty($secondary_links)){ print theme('links', $secondary_links, array('id' => 'secondary', 'class' => 'links sub-menu')); } ?>
-          </div> <!-- /navigation -->
+        <?php if ($links): ?>
+        <div class="links"> <?php //print $links; ?></div>
         <?php endif; ?>
-<!--           </div>  /content-inner /content -->
+
+    </div> <!-- /node-inner -->
+</div> <!-- /node-->
